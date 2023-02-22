0b5d9450eeab8b5377f1d54cab49c7428db3d3b8
Chris Edwards <chrisedwards96@outlook.com>
2023-02-22 21:38:01 +0000

    inital commit with progress from mob programming

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { Cat } = require("./models");
+
+const app = express();
+app.use(express.json());
+
+app.post("/cats", (req, res) => {
+  Cat.create(req.body).then((cat) => res.status(201).json(cat));
+});
+
+module.exports = app;
